e1d73d2ce4f92f9bd3df775091c2e70c2e2cf593
ciulinuwu <79811506+ciulinuwu@users.noreply.github.com>
2022-08-19 19:07:07 +0200

    new languages

---
+++ translations/português_brasileiro.js
@@ -0,0 +1,142 @@
+/* jshint esversion: 10 */
+const NAMESPACE_MODULE_CIULINATIONS_PTBR = "Ciulinations_Português_Brasileiro";
+
+window[NAMESPACE_MODULE_CIULINATIONS_PTBR] = {};
+(function translation() {
+    var language = "ptbr";
+    var author = "micenest";
+
+    const json = {
+        global: {
+            browse: "Navegar",
+            upload: "Enviar", 
+            help: "Ajuda",
+            about: "Sobre",
+            copyright: "Copyright",
+            advertising: "Publicidade", 
+            loading: "Carregando...",
+            channel: "Canal",
+            seeall: "ver tudo",
+            search: "Pesquisar",
+            browsechannels: "Pesquisar Canais",
+            press: "Imprensa e Blogs", 
+            creators: "Criadores e Parceiros",
+        },
+        channels: {
+            about: "Sobre %s"
+        },
+        stats: {
+            likesdislikes: "%s gostei, %r não gostei"
+        },
+        buttons: {
+            like: "Gostei",
+            addto: "Adicionar para",
+            share: "Compartilhar",
+            showmore: "Mostrar mais",
+            showless: "Mostrar menos",
+            subscribe: "Inscrever-se",
+            subscribed: "Inscrito",
+            unsubscribe: "Desinscrever",
+            post: "Enviar",
+            loadmoresuggestions: "Carregar mais sugestões",
+            signin: "Entrar"
+        },
+        suggestions: {
+            loading: "Carregando mais sugestões..."
+        },
+        comments: {
+            charactersremain: "%s caractéres restantes",
+            secondsremain: "Faltam %s segundos antes de você poder enviar.",
+            loading: "Carregando comentário...",
+            allcomments: "Todos os Comentários",
+            topcomments: "Comentários Populares",
+            respond: "Responda á esse vídeo..."
+        },
+        tooltip: {
+            ilikethis: "Gostei",
+            idislikethis: "Não Gostei",
+            addto: "Adiciona ao seus favoritos, ou á uma playlist.",
+            share: "Compartilhe este video",
+            flag: "Marque como inapropriado",
+            statistics: "Mostre estátisticas do vídeo",
+            clicktobe: "Clique para ser notificado de mais vídeos deste canal.",
+            interactive: "Transcrição Interativa"
+        },
+        player: {
+            mute: "Silenciar",
+            unmute: "Não Silenciar",
+            quality: "Mude a Qualidade",
+            fullscreen: "Tela Cheia"
+        },
+        watch: {
+            tags: "Tags",
+            license: "Licensa",
+            uploaderinfo: "Enviado por by %s em %r",
+            category: "Categoria",
+            from: "De: %s",
+            uploadedavideo: "%s enviou um video",
+            islive: "%s está ao vivo",
+            by: "de %s"
+        },    
+        uploadmonth: {
+            jan: "Jan",
+            feb: "Fev",
+            mar: "Mar",
+            apr: "Abr",
+            may: "Mai",
+            jun: "Jun",
+            jul: "Jul",
+            aug: "Ago",
+            sep: "Set",
+            oct: "Out",
+            nov: "Nov",
+            dec: "Dez"
+        },
+        about: {
+            country: "País:",
+            membersince: "Entrou:",
+            viewedcounts: "Visualizações:",
+            viewedcount: "Visto:",
+            sub: "Inscrito em:",
+            subs: "Inscritos:"
+        },
+        customtag: {
+            age: "Idade:",
+            occupation: "Ocupação:"
+        },
+        personal: {
+            mychannel: "Meu Canal",
+            videos: "Vídeos",
+            likes: "Curtidas",
+            history: "Histórico",
+            watchlater: "Assista Mais Tarde",
+            videomanager: "Gerenciador de Vídeos",
+            youtubesettings: "Configurações do YouTube",
+            subscriptions: "Inscrições",
+            profile: "Perfil",
+            privacy: "Privácidade",
+            settings: "Configurações",
+            signout: "Sair",
+            switchaccount: "Trocar de Conta",
+            googleaccount: "Conta do Google"
+        },
+        browse: {
+            mostviewed: "Mais vistos hoje",
+            recommended: "Recomendado para Você"
+        },
+        guide: {
+            fromyt: "Do YouTube",
+            trending: "Em Alta",
+            popular: "Popular",
+            music: "Música",
+            live: "Ao Vivo",
+            gaming: "Jogos",
+            news: "Notícias",
+            sports: "Esportes",
+            education: "Educação",
+            howto: "Tutoriais e Estilo" 
+        }
+    };
+    
+    Ciulinations.addTranslation(language, json, author);
+})(NAMESPACE_MODULE_CIULINATIONS_PTBR);
